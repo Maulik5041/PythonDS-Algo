c3b4cdfa1d2be5a127090486b96f462d6a7de9e4
Maulik Padh <maulik.padh93@gmail.com>
2020-05-05 05:09:46 -0400

    Create breadth_first_traversal_binary_tree.py

---
+++ Binary Tree/breadth_first_traversal_binary_tree.py
@@ -0,0 +1,132 @@
+"""implementation of Breadth-First Traversal in Binary Tree"""
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        return self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
+
+    def __len__(self):
+        return self.size()
+
+    def size(self):
+        return len(self.items)
+
+
+class BinaryTree:
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def print_tree(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root, "")
+
+        if traversal_type == "inorder":
+            return self.inorder_print(tree.root, "")
+
+        if traversal_type == "postorder":
+            return self.postorder_print(tree.root, "")
+
+        if traversal_type == "levelorder":
+            return self.levelorder_print(tree.root)
+
+        print(f"The traversal type {(str(traversal_type))} is not supported.")
+        return False
+
+    def preorder_print(self, start, traversal):
+        if start:
+            traversal += (str(start.value) + " - ")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+
+        return traversal
+
+    def inorder_print(self, start, traversal):
+        if start:
+            traversal = self.inorder_print(start.left, traversal)
+            traversal += (str(start.value) + " - ")
+            traversal = self.inorder_print(start.right, traversal)
+
+        return traversal
+
+    def postorder_print(self, start, traversal):
+        if start:
+            traversal = self.postorder_print(start.left, traversal)
+            traversal = self.postorder_print(start.right, traversal)
+            traversal += (str(start.value) + " - ")
+
+        return traversal
+
+    def levelorder_print(self, start):
+        if start is None:
+            return
+
+        queue = Queue()
+        queue.enqueue(start)
+
+        traversal = ""
+        while len(queue) > 0:
+            traversal += str(queue.peek()) + " - "
+            node = queue.dequeue()
+
+            if node.left:
+                queue.enqueue(node.left)
+
+            if node.right:
+                queue.enqueue(node.right)
+
+        return traversal
+
+
+# 1-2-4-5-3-6-7-
+# 4-2-5-1-6-3-7-
+# 4-2-5-6-3-7-1-
+# 1-2-3-4-5-6-7-
+
+#             1
+#            /  \
+#           /    \
+#          2      3
+#         /  \   / \
+#        4    5 6   7
+
+# Set up tree:
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
+
+
+print("------------Pre-order Traversal-------------")
+print(tree.print_tree("preorder"), "\n")
+
+print("-------------In-order Traversal------------")
+print(tree.print_tree("inorder"), "\n")
+
+print("--------------Post-order Traversal-----------")
+print(tree.print_tree("postorder"), "\n")
+
+print("--------------Level-order Traversal-----------")
+print(tree.print_tree("levelorder"))
