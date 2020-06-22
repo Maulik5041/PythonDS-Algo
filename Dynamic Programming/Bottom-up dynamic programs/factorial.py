f7018d2dc05a2735f71351f0d23f86dbe9624a68
Maulik Padh <maulik.padh93@gmail.com>
2020-06-22 03:08:26 -0400

    Create factorial.py

---
+++ Dynamic Programming/Bottom-up dynamic programs/factorial.py
@@ -0,0 +1,20 @@
+"""Factorial of n using bottom-up approach"""
+
+
+def factorial(n):
+    # tabulation table of size n+1
+    table = [0] * (n + 1)
+
+    # base case of 0! = 1
+    table[0] = 1
+
+    # iterate until n
+    for i in range(1, n+1):
+        # using answer to i-1th problem to build for ith problem
+        table[i] = table[i - 1] * i
+
+    # return answer; the nth factorial
+    return table[n]
+
+
+print(factorial(30))
